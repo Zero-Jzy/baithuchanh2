7e6f0d345b748e8e43618014b5ac6d5097e1caa3
phu <dekujzy@gmail.com>
2018-11-10 18:01:55 +0700

    4

--- js/myjs.js
+++ js/myjs.js
@@ -1,6 +1,8 @@
 var btnSubmit = document.forms[0]["btn-submit"];
-var content='';
-var a;
+var content = '';
+var e=false;
+var b=false;
+var c=false;
 btnSubmit.onclick = function () {
     var txtusername = document.forms[0]["username"];
     var msgUsername = txtusername.nextElementSibling;
@@ -13,13 +15,13 @@ btnSubmit.onclick = function () {
         msgUsername.classList.remove("success-text");
         msgUsername.classList.add("danger-text");
         msgUsername.classList.remove("hidden-text");
-    }else {
+    } else {
         msgUsername.innerHTML = "Tên hợp lệ.";
         msgUsername.classList.remove("danger-text");
         msgUsername.classList.add("success-text");
         msgUsername.classList.remove("hidden-text");
-        content+='ten nguoi dung:'+txtusername.value+'<br>';
-        a=true;
+        content += 'ten nguoi dung:' + txtusername.value + '<br>';
+        e = true;
     }
     var txtpassword = document.forms[0]["phone"];
     var msgpassword = txtpassword.nextElementSibling;
@@ -33,45 +35,46 @@ btnSubmit.onclick = function () {
         msgpassword.classList.remove("danger-text");
         msgpassword.classList.add("success-text");
         msgpassword.classList.remove("hidden-text");
-        content+='sdt nguoi dung:'+txtpassword.value+'<br>';
-        a=true;
+        content += 'sdt nguoi dung:' + txtpassword.value + '<br>';
+        b = true;
     }
     var txtEmail = document.forms[0]["email"];
     var msgEmail = txtEmail.nextElementSibling;
-    var a=txtEmail.value.includes("@");
+    var a = txtEmail.value.includes("@");
     if (txtEmail == null || txtEmail.value.length === 0) {
         msgEmail.innerHTML = "Vui Long nhap email!";
         msgEmail.classList.remove("success-text");
         msgEmail.classList.add("danger-text");
         msgEmail.classList.remove("hidden-text");
     }
-    else if (a===false){
+    else if (a === false) {
         msgEmail.innerHTML = "Sai cú pháp email!";
         msgEmail.classList.remove("success-text");
         msgEmail.classList.add("danger-text");
         msgEmail.classList.remove("hidden-text");
     }
-    else{
+    else {
         msgEmail.innerHTML = "Email hợp lệ.";
         msgEmail.classList.remove("danger-text");
         msgEmail.classList.add("success-text");
         msgEmail.classList.remove("hidden-text");
-        content+='email nguoi dung'+txtEmail.value+'<br>';
-        a=true;
+        content += 'email nguoi dung' + txtEmail.value + '<br>';
+        c = true;
     }
     var gender = document.forms[0]["gender"].value;
     var sothich = document.forms[0]["a"];
-    for (const b of sothich) {
-        if (b.checked) {
-            sothich = b.value
-        }
-    }
-    var goithieu = document.forms[0]["textarea"].value;
+    var txtsothich = '';
+    for (const d of sothich) {
+        if (d.checked) {
+txtsothich += d.value + ' ';
+}
+}
+var goithieu = document.forms[0]["textarea"].value;
 
-    content+='gender:'+gender+'<br>';
-    content+='so thich:'+sothich+'<br>';
-    content+='gioi thieu:'+goithieu+'<br>';
-    if (a===true){
-        document.getElementsByClassName('text2')[0].innerHTML=content;
-    }
+content += 'gender:' + gender + '<br>';
+content += 'so thich:' + txtsothich + '<br>';
+content += 'gioi thieu:' + goithieu + '<br>';
+if (e === true && b === true && c === true) {
+    document.getElementsByClassName('text2')[0].innerHTML = content;
+}
 };
